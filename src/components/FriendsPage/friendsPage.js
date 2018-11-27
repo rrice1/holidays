a497120f414f0697918df09833c5ff522ebd3f8e
Robert Rice <rrice1291@gmail.com>
2018-11-26 19:48:58 -0600

    add

--- src/components/FriendsPage/friendsPage.js
+++ src/components/FriendsPage/friendsPage.js
@@ -1,8 +1,18 @@
 import $ from 'jquery';
 import authHelpers from '../../helpers/authHelpers';
 import friendsData from '../../helpers/data/friendsData';
+import holidayFriendsData from '../../helpers/data/holidayFriendsData';
+import holidaysData from '../../helpers/data/holidaysData';
 
-const printSingleFriend = (friend) => {
+const holidayStringBuilder = (holidays) => {
+  let holidayString = '<h3>Holidays:</h3>';
+  holidays.forEach((holiday) => {
+    holidayString += `<h5>${holiday.name} ${holiday.Date}</h5>`;
+  });
+  return holidayString;
+};
+
+const printSingleFriend = (friend, holidays) => {
   const friendString = `
   <div>
     <h1>${friend.name}</h1>
@@ -12,6 +22,7 @@ const printSingleFriend = (friend) => {
     <p>${friend.phoneNumber}</p>
     <button class="btn btn-danger delete-btn" data-delete-id=${friend.id}>X</button>
     <button class="btn btn-info edit-btn" data-edit-id=${friend.id}>Edit</button>
+    <div class="holiday-container">${holidayStringBuilder(holidays)}</div>
     </div>
   `;
   $('#single-container').html(friendString);
@@ -20,9 +31,17 @@ const printSingleFriend = (friend) => {
 const getSingleFriend = (e) => {
   // firebase id
   const friendId = e.target.dataset.dropdownId;
+  const uid = authHelpers.getCurrentUid();
   friendsData.getSingleFriend(friendId)
     .then((singleFriend) => {
-      printSingleFriend(singleFriend);
+      holidayFriendsData.getHolidayIdsForFriend(friendId).then((holidayIds) => {
+        console.log('holidayIds', holidayIds);
+        holidaysData.getHolidaysByArrayOfIds(uid, holidayIds).then((holidays) => {
+          printSingleFriend(singleFriend, holidays);
+        });
+      });
+      // const holidaysIds = ['holiday1', 'holiday2'];
+      // const holidays = ['a', 'b', 'c'];
     })
     .catch((error) => {
       console.error('error in getting one friend', error);
